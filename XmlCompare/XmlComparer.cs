06a12b2568ca55917e789f3a37b1cff2d4e362a1
wierichs <gerd@wierichs.org>
2018-03-19 16:14:16 +0100

    Initial running version
    with bugs

--- XmlCompare/XmlComparer.cs
+++ XmlCompare/XmlComparer.cs
@@ -7,108 +7,175 @@ using System.Xml.Linq;
 
 namespace XmlCompare
 {
-    class XmlComparer
+  class XmlComparer
+  {
+    private XDocument m_docBase = new XDocument();
+    private XDocument m_docCompare = new XDocument();
+    private XDocument m_docResult = new XDocument();
+    private string m_attribIdentifier = "ID";
+
+    public XmlComparer(XDocument xBaseDocument, XDocument xDocumentToCompare, string IdentifyingAttribute)
     {
-        private XDocument m_docBase = new XDocument();
-        private XDocument m_docCompare = new XDocument();
-        private XDocument m_docResult = new XDocument();
-        private string m_attribIdentifier = "ID";
-
-        public XmlComparer(XDocument xBaseDocument, XDocument xDocumentToCompare, string IdentifyingAttribute)
-        {
-            m_docBase = xBaseDocument;
-            m_docCompare = xDocumentToCompare;
-            m_attribIdentifier = IdentifyingAttribute;
-        }
-        public XmlComparer(string BaseDocument, string DocumentToCompare)
-        {
-            m_docBase = XDocument.Load(BaseDocument, LoadOptions.PreserveWhitespace);
-            m_docCompare = XDocument.Load(DocumentToCompare, LoadOptions.PreserveWhitespace);
-        }
-
-        public XDocument ResultDocument { get { return m_docResult; } }
-
-        private void CompareDocuments()
-        {
-            m_docResult = new XDocument();
-
-            m_docResult.Add(new XElement(m_docBase.Root.Name));
-            CompareElements(m_docResult.Root, m_docBase.Root, m_docCompare.Root);
-        }
-
-        private void CompareElements(XElement xResultElement, XElement xBaseElment, XElement xElementToCompare)
-        {
-            foreach(XNode xNode in xBaseElment.Nodes())
-            {
-                switch(xNode.NodeType)
-                {
-                    case System.Xml.XmlNodeType.Text:
-                        XText xBaseText = (XText)xNode;
-                        CompareText(xResultElement, xBaseText, xElementToCompare);
-                        break;
-                    case System.Xml.XmlNodeType.Element:
-                        XElement xBaseSubElement = (XElement)xNode;
-                        xResultElement.Add(new XElement(xBaseSubElement.Name));
-                        XElement xResultSubElement = (XElement)xResultElement.LastNode;
-                        XElement xCompareSub = GetSubElement(xBaseSubElement, xElementToCompare);
-                        CompareElements(xResultSubElement, xBaseSubElement, xElementToCompare);
-                        break;
-                }
-            }
-            foreach(XAttribute xBaseAttribute in xBaseElment.Attributes())
-            {
-                CompareAttributes(xResultElement, xBaseAttribute, xElementToCompare);
-            }
-        }
-
-        private XElement GetSubElement(XElement xReference, XElement xElementToSearch)
-        {
-            XElement xResult = null;
-            foreach (XElement xSub in xElementToSearch.Elements().Where(e => e.Name.Equals(xReference.Name)))
-            {
-                var xAttIdent = xSub.Attributes().Where(a => a.Name.Equals(m_attribIdentifier)).FirstOrDefault();
-                if (xAttIdent == null)
-                {
-                    xAttIdent = xSub.Attributes().Where(a => a.Name.Equals("ID")).FirstOrDefault();
-                }
-                if (xAttIdent == null)
-                {
-                    xAttIdent = xSub.Attributes().Where(a => a.Name.Equals("Name")).FirstOrDefault();
-                }
-                if (xAttIdent == null)
-                {
-                }
-            }
-            return xResult;
-        }
-
-        private void CompareText(XElement xResultElement, XText xBaseText, XElement xElementToCompare)
-        {
-            if(!string.IsNullOrWhiteSpace(xBaseText.Value.Trim()))
-            {
-                var xTexts = xElementToCompare.Nodes().Where(n => n.NodeType == System.Xml.XmlNodeType.Text);
-                foreach (XText xtCompare in xTexts)
-                {
-                    if(xtCompare.Value.Equals(xBaseText.Value))
-                    {
-                        return;
-                    }
-                }
-                xResultElement.Add(new XAttribute("MissingText", xBaseText.Value));
-            }
-
-        }
-
-        private void CompareAttributes(XElement xResultElement, XAttribute xBaseAttribute, XElement xElementToCompare)
-        {
-            XAttribute xaResultMissing = xResultElement.Attribute("MissingAttribute");
-            XAttribute xaResultDiffers = xResultElement.Attribute("DifferentAttribute");
-            XAttribute xaCompare = new XAttribute(xBaseAttribute.Name, xResultElement.Attribute(xBaseAttribute.Name).Value);
-
-            if (xaCompare == null)
-            {
-                xaResultMissing.Value += string.Format("[{0}={1}]", xBaseAttribute.Name, xBaseAttribute.Value);
-            }
-        }
+      m_docBase = xBaseDocument;
+      m_docCompare = xDocumentToCompare;
+      m_attribIdentifier = IdentifyingAttribute;
+      CompareDocuments();
     }
+    public XmlComparer(string BaseDocument, string DocumentToCompare, string IdentifyingAttribute)
+    {
+      m_docBase = XDocument.Load(BaseDocument, LoadOptions.PreserveWhitespace);
+      m_docCompare = XDocument.Load(DocumentToCompare, LoadOptions.PreserveWhitespace);
+      m_attribIdentifier = IdentifyingAttribute;
+      CompareDocuments();
+    }
+    public XmlComparer(XDocument xBaseDocument, XDocument xDocumentToCompare)
+    {
+      m_docBase = xBaseDocument;
+      m_docCompare = xDocumentToCompare;
+      CompareDocuments();
+    }
+    public XmlComparer(string BaseDocument, string DocumentToCompare)
+    {
+      m_docBase = XDocument.Load(BaseDocument, LoadOptions.PreserveWhitespace);
+      m_docCompare = XDocument.Load(DocumentToCompare, LoadOptions.PreserveWhitespace);
+      CompareDocuments();
+    }
+
+    public XDocument ResultDocument { get { return m_docResult; } }
+
+    private void CompareDocuments()
+    {
+      m_docResult = new XDocument(new XProcessingInstruction("xml-stylesheet", "version=\"1.0\" xmlns:xsl=\"http://www.w3.org/1999/XSL/Transform\""), new XElement(m_docBase.Root.Name));
+      CompareElements(m_docResult.Root, m_docBase.Root, m_docCompare.Root);
+    }
+
+    private void CompareElements(XElement xResultElement, XElement xBaseElement, XElement xElementToCompare)
+    {
+      foreach (XNode xNode in xBaseElement.Nodes())
+      {
+        switch (xNode.NodeType)
+        {
+          case System.Xml.XmlNodeType.Text:
+            if (xElementToCompare != null)
+            {
+              XText xBaseText = (XText)xNode;
+              CompareText(xResultElement, xBaseText, xElementToCompare);
+            }
+            break;
+          case System.Xml.XmlNodeType.Element:
+            XElement xBaseSubElement = (XElement)xNode;
+            xResultElement.Add(new XElement(xBaseSubElement.Name));
+            XElement xResultSubElement = (XElement)xResultElement.LastNode;
+            XElement xCompareSub = GetSubElement(xBaseSubElement, xElementToCompare);
+            CompareElements(xResultSubElement, xBaseSubElement, xCompareSub);
+            break;
+        }
+      }
+      CompareAttributes(xResultElement, xBaseElement, xElementToCompare);
+    }
+
+    private XElement GetSubElement(XElement xReference, XElement xElementToSearch)
+    {
+      XElement xResult = null;
+      string sIdentifier = m_attribIdentifier;
+      if (xElementToSearch != null)
+      {
+        if (xReference.Attribute(sIdentifier) == null)
+        {
+          sIdentifier = "ID";
+          if (xReference.Attribute(sIdentifier) == null)
+          {
+            sIdentifier = "Name";
+            if (xReference.Attribute(sIdentifier) == null)
+            {
+              if (xReference.Attributes().FirstOrDefault() != null)
+              {
+                sIdentifier = xReference.Attributes().FirstOrDefault().Name.ToString();
+              }
+              if (xResult == null && xElementToSearch != null)
+              {
+                xResult = xElementToSearch.Elements().ElementAt(xReference.ElementsBeforeSelf().Count());
+              }
+            }
+          }
+        }
+      }
+      if(xResult == null && xElementToSearch != null)
+      {
+        xResult = xElementToSearch.Elements()
+                  .Where(e => e.Name.Equals(xReference.Name)
+                  && e.Attribute(sIdentifier) != null
+                  && e.Attribute(sIdentifier).Value.Equals(xReference.Attribute(sIdentifier).Value)).FirstOrDefault();
+      }
+      return xResult;
+    }
+
+    private void CompareText(XElement xResultElement, XText xBaseText, XElement xElementToCompare)
+    {
+      if(xElementToCompare == null)
+      {
+        xResultElement.Add(new XAttribute("MissingText", xBaseText.Value));
+      }
+      else if (!string.IsNullOrWhiteSpace(xBaseText.Value.Trim()))
+      {
+        var xTexts = xElementToCompare.Nodes().Where(n => n.NodeType == System.Xml.XmlNodeType.Text);
+        foreach (XText xtCompare in xTexts)
+        {
+          if (xtCompare.Value.Equals(xBaseText.Value))
+          {
+            return;
+          }
+        }
+        xResultElement.Add(new XAttribute("MissingText", xBaseText.Value));
+      }
+
+    }
+
+    private void CompareAttributes(XElement xResultElement, XElement xBaseElement, XElement xElementToCompare)
+    {
+      XAttribute xaResultMissing = xResultElement.Attribute("MissingAttribute");
+      XAttribute xaResultDiffers = xResultElement.Attribute("DifferentAttribute");
+      XAttribute xaResultAdditional = xResultElement.Attribute("AdditionalAttribute");
+      if (xaResultMissing == null) { xaResultMissing = new XAttribute("MissingAttribute", string.Empty); }
+      if (xaResultDiffers == null) { xaResultDiffers = new XAttribute("DifferentAttribute", string.Empty); }
+      if (xaResultAdditional == null) { xaResultAdditional = new XAttribute("AdditionalAttribute", string.Empty); }
+      foreach (XAttribute xBaseAttribute in xBaseElement.Attributes())
+      {
+        xResultElement.Add(new XAttribute(xBaseAttribute.Name, xBaseAttribute.Value));
+        if (xElementToCompare == null)
+        {
+          xaResultMissing.Value += string.Format("[{0}={1}]", xBaseAttribute.Name, xBaseAttribute.Value);
+        }
+        else
+        {
+          XAttribute xaCurrent = xElementToCompare.Attribute(xBaseAttribute.Name);
+
+          if (xaCurrent == null)
+          {
+            xaResultMissing.Value += string.Format("[{0}={1}]", xBaseAttribute.Name, xBaseAttribute.Value);
+          }
+          else
+          {
+            XAttribute xaCompare = new XAttribute(xBaseAttribute.Name, xaCurrent.Value);
+            if (!xBaseAttribute.Value.Equals(xaCompare.Value))
+            {
+              xaResultDiffers.Value += string.Format("[{0}={1}]", xBaseAttribute.Name, xBaseAttribute.Value);
+            }
+          }
+        }
+      }
+
+      if (xElementToCompare != null)
+      {
+        foreach (XAttribute xCompareAttribute in xElementToCompare.Attributes())
+        {
+          if (xBaseElement.Attribute(xCompareAttribute.Name) == null)
+          {
+            xaResultAdditional.Value += string.Format("[{0}={1}]", xCompareAttribute.Name, xCompareAttribute.Value);
+            xResultElement.Add(new XAttribute(xCompareAttribute.Name, xCompareAttribute.Value));
+          }
+        }
+      }
+    }
+  }
 }
--- XmlCompare/frmXmlCompare.cs
+++ XmlCompare/frmXmlCompare.cs
@@ -8,59 +8,91 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml;
 using System.Xml.Linq;
 
 namespace XmlCompare
 {
-    public partial class frmXmlCompare : Form
+  public partial class frmXmlCompare : Form
+  {
+    public frmXmlCompare()
     {
-        public frmXmlCompare()
-        {
-            InitializeComponent();
-            this.Text = string.Format("{0} ({1})", Application.ProductName, Application.ProductVersion.ToString());
-            cboFilter.SelectedIndex = 0;
-            cboIdentifier.SelectedIndex = 0;
-        }
-
-        private void btnBaseFile_Click(object sender, EventArgs e)
-        {
-            if(dlgOpenFile.ShowDialog() == DialogResult.OK)
-            {
-                txtBaseFile.Text = dlgOpenFile.FileName;
-            }
-        }
-
-        private void btnFileToCompare_Click(object sender, EventArgs e)
-        {
-            if (dlgOpenFile.ShowDialog() == DialogResult.OK)
-            {
-                txtFileToCompare.Text = dlgOpenFile.FileName;
-            }
-        }
-
-        private void btnCompare_Click(object sender, EventArgs e)
-        {
-            string sMessageText = string.Empty;
-            if (!File.Exists(txtBaseFile.Text))
-            {
-                sMessageText = string.Format("{0} ({1}) doesn't exist!"
-                    , lblBaseFile.Text.Substring(0, lblBaseFile.Text.Length - 1)
-                    , txtBaseFile.Text);
-                MessageBox.Show(sMessageText);
-                return;
-            }
-            if (!File.Exists(txtFileToCompare.Text))
-            {
-                sMessageText = string.Format("{0} ({1}) doesn't exist!"
-                    , lblFileToCompare.Text.Substring(0, lblFileToCompare.Text.Length - 1)
-                    , txtFileToCompare.Text);
-                MessageBox.Show(sMessageText);
-                return;
-            }
-
-            XDocument xBase = XDocument.Load(txtBaseFile.Text);
-            webResult.DocumentText = xBase.Document.ToString();
-            webResult.Refresh();
-        }
+      InitializeComponent();
+      this.Text = string.Format("{0} ({1})", Application.ProductName, Application.ProductVersion.ToString());
+      cboFilter.SelectedIndex = 0;
+      cboIdentifier.SelectedIndex = 0;
     }
+
+    private void btnBaseFile_Click(object sender, EventArgs e)
+    {
+      if (dlgOpenFile.ShowDialog() == DialogResult.OK)
+      {
+        txtBaseFile.Text = dlgOpenFile.FileName;
+        cboIdentifier.Items.Clear();
+        cboIdentifier.Items.AddRange(GetAttributeNames(XDocument.Load(txtBaseFile.Text).Root));
+        if (cboIdentifier.Items.Contains("ID")) { cboIdentifier.SelectedIndex = cboIdentifier.Items.IndexOf("ID"); }
+        else if (cboIdentifier.Items.Contains("Name")) { cboIdentifier.SelectedIndex = cboIdentifier.Items.IndexOf("Name"); }
+      }
+    }
+
+    private string[] GetAttributeNames(XElement xElement)
+    {
+      List<string> slAttributes = new List<string>();
+      slAttributes.AddRange(xElement.Attributes().Select(a => a.Name.ToString()));
+      foreach (XElement xSub in xElement.Elements())
+      {
+        slAttributes.AddRange(GetAttributeNames(xSub).Where(i => !slAttributes.Contains(i)));
+      }
+      return slAttributes.ToArray();
+    }
+
+    private void btnFileToCompare_Click(object sender, EventArgs e)
+    {
+      if (dlgOpenFile.ShowDialog() == DialogResult.OK)
+      {
+        txtFileToCompare.Text = dlgOpenFile.FileName;
+      }
+    }
+
+    private void btnCompare_Click(object sender, EventArgs e)
+    {
+      string sMessageText = string.Empty;
+      if (!File.Exists(txtBaseFile.Text))
+      {
+        sMessageText = string.Format("{0} ({1}) doesn't exist!"
+            , lblBaseFile.Text.Substring(0, lblBaseFile.Text.Length - 1)
+            , txtBaseFile.Text);
+        MessageBox.Show(sMessageText);
+        return;
+      }
+      if (!File.Exists(txtFileToCompare.Text))
+      {
+        sMessageText = string.Format("{0} ({1}) doesn't exist!"
+            , lblFileToCompare.Text.Substring(0, lblFileToCompare.Text.Length - 1)
+            , txtFileToCompare.Text);
+        MessageBox.Show(sMessageText);
+        return;
+      }
+
+      XDocument xBase = XDocument.Load(txtBaseFile.Text);
+      XDocument xCompare = XDocument.Load(txtFileToCompare.Text);
+
+      XmlComparer xComparer = new XmlComparer(xBase, xCompare, cboIdentifier.Text);
+
+      System.Xml.Xsl.XslCompiledTransform xTrans = new System.Xml.Xsl.XslCompiledTransform();
+      xTrans.Load(".\\Resources\\Defaults.xslt");
+      StringReader sr = new StringReader(xComparer.ResultDocument.ToString());
+      XmlReader xReader = XmlReader.Create(sr);
+
+      //Apply / transform the XML data
+      System.IO.MemoryStream ms = new MemoryStream();
+      xTrans.Transform(xReader, null, ms);
+
+      // Reset the position
+      ms.Position = 0;
+
+      webResult.DocumentStream = ms;
+      webResult.Refresh();
+    }
+  }
 }
